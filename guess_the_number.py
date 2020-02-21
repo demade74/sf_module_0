28143c873dcb8938da27cc147bc27f08180fe6ac
demade74 <demade74@gmail.com>
2020-02-21 14:20:25 +0300

    Реализация игры "Угадай число"

---
+++ guess_the_number.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+# Так как по условию задачи известно, что загадывается число от 1 до 100,
+# то угадывание значения хорошо решается алгоритмом бинарного поиска
+# Параметр number - загаданное число
+
+
+def game(number):
+    predict = 0
+    attempts_count = 0
+    line = [i for i in range(1, 101)]
+    l, r = 0, len(line) - 1
+
+    while l <= r:
+        middle = l + (r - l) // 2
+
+        if line[middle] == number:
+            predict = line[middle]
+            break
+
+        if line[middle] < number:
+            attempts_count += 1
+            l = middle + 1
+            predict = line[middle]
+
+        else:
+            attempts_count += 1
+            r = middle - 1
+            predict = line[middle]
+
+    print(f'Было загадано число {predict}. Отгадано за {attempts_count} попыток.')
+
+
+# Тест
+game(15)
+game(88)
+game(37)
+game(1)
+game(50)
+game(100)
+game(61)
+
+# Результат
+print('\n')
+game(np.random.randint(1, 101))
+
+
+
+
+
