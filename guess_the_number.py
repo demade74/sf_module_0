53876eef11301a5ed8ad5e86d776611732cad22c
demade74 <demade74@gmail.com>
2020-03-12 20:14:16 +0300

    small edits

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-# Так как по условию задачи известно, что загадывается число от 1 до 100,
-# то угадывание значения хорошо решается алгоритмом бинарного поиска
+
 # Параметр number - загаданное число
 
 
 def game(number):
     predict = 0
     attempts_count = 0
-    line = [i for i in range(1, 101)]
+    line = list(range(1, 101))
     l, r = 0, len(line) - 1
 
     while l <= r:
@@ -22,17 +21,18 @@ def game(number):
         if line[middle] < number:
             attempts_count += 1
             l = middle + 1
-            predict = line[middle]
+            predict = line[l + (r - l) // 2]
 
         else:
             attempts_count += 1
             r = middle - 1
-            predict = line[middle]
+            predict = line[l + (r - l) // 2]
 
     print(f'Было загадано число {predict}. Отгадано за {attempts_count} попыток.')
 
 
 # Тест
+game(25)
 game(15)
 game(88)
 game(37)
@@ -40,6 +40,9 @@ game(1)
 game(50)
 game(100)
 game(61)
+game(59)
+game(33)
+game(77)
 
 # Результат
 print('\n')
